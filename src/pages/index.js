68b99c90a83fb9adce80fda3941a23e67a6204e2
satyendrakannaujiya <satyendra250896@gmail.com>
2018-10-31 00:18:38 +0530

    add my education

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>My name is satyendra kannaujiya</p>
     <p>I am a web developer</p>
+    <p>I am persuing master of computer application from NIT Allahabad,(UP) INDIA</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
